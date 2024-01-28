f04f3f95a808ee2dd0c6a45056c53c654097e06a
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2024-01-28 09:15:16 +0100

    baby gd

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -1,31 +1,16 @@
 class_name StateMachine
 extends Node
  
-@export var current_state: State
-var states: Dictionary = {}
- 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var scoreNode = get_node("/root/Score")
+
 func _ready():
-	for child in get_children():
-		if child is State:
-			states[child.name] = child
-			child.transitioned.connect(on_child_transitioned)
-		else:
-			push_warning("State machine contains child which is not 'State'")
-			
-	current_state.Enter()
-			
+	pass
+
 func _process(delta):
-	current_state.Update(delta)
-		
-func _physics_process(delta):
-	current_state.Physics_update(delta)
-	
-func on_child_transitioned(new_state_name: StringName) -> void:
-	var new_state = states.get(new_state_name)
-	if new_state != null:
-		if new_state != current_state:
-			current_state.Exit()
-			new_state.Enter()
-			current_state = new_state
+	if scoreNode.points <= 5:
+		animated_sprite_2d.play('cry')
+	elif scoreNode.points <= 10:
+		animated_sprite_2d.play('smile')
 	else:
-		push_warning("Called transition on a state that does not exist")
+		animated_sprite_2d.play('laugh')
